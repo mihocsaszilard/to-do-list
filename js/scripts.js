e3dfe9d76f7ca98fa3d353d1e60c77e21d80bf21
Mihocsa Szilard <mihocsa48@gmail.com>
2021-06-14 19:17:42 +0200

    adding delete button to li + beautify

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,17 @@ function newItem() {
     li.toggleClass('strike');
   });
 
-  
+  //adding the delete button
+  const crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  //adding class delete from css
+  crossOutButton.on('click', deleteListItem)
+
+  function deleteListItem() {
+    li.addClass('delete')
+  };
+
   $('#list').sortable();
 }
